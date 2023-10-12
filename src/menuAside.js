ff78966ca56138b9b95f8a9d8970308e3f86614c
Edgarmad <54693845+Edgarmad@users.noreply.github.com>
2023-10-12 14:52:50 -0600

    first commit

---
+++ src/menuAside.js
@@ -0,0 +1,24 @@
+import {
+  mdiAccountCircle,
+  mdiMonitor,
+  mdiLock,
+} from '@mdi/js'
+
+export default [
+  {
+    to: '/',
+    icon: mdiMonitor,
+    label: 'Dashboard'
+  },
+  {
+    to: '/profile',
+    label: 'Profile',
+    icon: mdiAccountCircle
+  },
+  {
+    to: '/login',
+    label: 'Login',
+    icon: mdiLock
+  },
+
+]
